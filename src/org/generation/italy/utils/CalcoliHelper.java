77c198499ae88a2fa6efb6e27a9358504102e511
leonardobernardi <lbernardi4298@gmail.com>
2021-11-29 17:02:58 +0100

    commit

---
+++ src/org/generation/italy/utils/CalcoliHelper.java
@@ -0,0 +1,105 @@
+package org.generation.italy.utils;
+
+public class CalcoliHelper {
+	
+	private CalcoliHelper() {
+		
+	}
+	
+	public static int somma (int a, int b) {
+		return a + b;
+	}
+	
+	public static double somma (double a, double b) {
+		return a + b;
+	}
+	
+	public static int differenza (int a, int b) {
+		return a - b;
+	}
+	
+	public static double differenza (double a, double b) {
+		return a - b;
+	}
+	
+	public static int moltiplicazione (int a, int b) {
+		return a * b;
+	}
+	
+	public static double moltiplicazione (double a, double b) {
+		return a * b;
+	}
+	
+	public static int valoreAssoluto (int a) {
+		if(a < 0) {
+			return a + a * -2;
+		}else {
+			return a;
+		}
+	}
+	
+	public static double valoreAssoluto (double a) {
+		if(a < 0) {
+			return a + a * -2;
+		}else {
+			return a;
+		}
+	}
+	
+	public static int minimo (int a, int b) {
+		if(a <= b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+		
+	public static double minimo (double a, double b) {
+		if(a <= b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+	
+	public static int massimo (int a, int b) {
+		if(a >= b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+	
+	public static double massimo (double a, double b) {
+		if(a >=b) {
+			return a;
+		}else{
+			return b;
+		}
+	}
+	
+	public static double potenza (int a, int b) {
+		int result = 0;
+		if(b == 0) {
+			result = 1;
+		}else if(b == 1) {
+			result = a;
+		}
+		else if(b < 0) {
+			a = 1 / a;
+			b = b + b * -2;
+			int c = a * a;
+			for(int i = 2; i < b; i++) {
+				c = c * a;
+			}
+			result = c;
+		}else{
+			int c = a * a;
+			for(int i = 2; i < b; i++) {
+				c = c * a;
+			}
+			result = c;
+		}
+		return result;
+	}
+}
---
+++ src/org/generation/italy/utils/Main.java
@@ -0,0 +1,23 @@
+package org.generation.italy.utils;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		System.out.println(CalcoliHelper.somma(10, 10));
+//		System.out.println(CalcoliHelper.somma(2.5, 2.5));
+//		System.out.println(CalcoliHelper.differenza(10, 5));
+//		System.out.println(CalcoliHelper.differenza(10, 2.5));
+//		System.out.println(CalcoliHelper.moltiplicazione(10, 10));
+//		System.out.println(CalcoliHelper.moltiplicazione(10, 1.5));
+//		System.out.println(CalcoliHelper.valoreAssoluto(0));
+//		System.out.println(CalcoliHelper.valoreAssoluto(-0.5));
+//		System.out.println(CalcoliHelper.minimo(10, 100));
+//		System.out.println(CalcoliHelper.minimo(1.5, 1.6));
+//		System.out.println(CalcoliHelper.massimo(10, 100));
+//		System.out.println(CalcoliHelper.massimo(1.5, 1.6));
+		System.out.println(CalcoliHelper.potenza(5, -2));
+		
+
+	}
+
+}
